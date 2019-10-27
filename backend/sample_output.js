ce74a7a6a5d3bffe739922b3665e1bc8b7d29a39
ashishkharel1 <55903660+ashishkharel1@users.noreply.github.com>
2019-10-27 08:15:50 -0400

    Add files via upload

---
+++ backend/sample_output.js
@@ -0,0 +1,41 @@
+
+<!-- saved from url=(0148)https://3000-dot-9011213-dot-devshell.appspot.com/name?firstname=I%20need%20a%20tall%20building%20in%20chicago.%20it%20should%20be%20red%20and%20big -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>{
+  "entities": [
+    {
+      "mentions": [
+        {
+          "text": {
+            "beginOffset": 14,
+            "content": "building"
+          },
+          "type": "COMMON"
+        }
+      ],
+      "metadata": {},
+      "name": "building",
+      "salience": 0.5810878,
+      "type": "LOCATION"
+    },
+    {
+      "mentions": [
+        {
+          "text": {
+            "beginOffset": 26,
+            "content": "chicago"
+          },
+          "type": "PROPER"
+        }
+      ],
+      "metadata": {
+        "mid": "/m/01_d4",
+        "wikipedia_url": "https://en.wikipedia.org/wiki/Chicago"
+      },
+      "name": "chicago",
+      "salience": 0.41891217,
+      "type": "LOCATION"
+    }
+  ],
+  "language": "en"
+}
+</body></html>
